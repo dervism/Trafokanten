dfb2e7d7b04a9da49572e063c4ca5f630030c604
dervis4 <dervis4@d18dd957-a13c-2806-2f7b-34b71d78d0bc>
2012-01-24 12:58:07 +0000

    code clean up & formatting

--- src/main/java/net/dervism/trafokanten/main/Trafokanten.java
+++ src/main/java/net/dervism/trafokanten/main/Trafokanten.java
@@ -1,40 +1,25 @@
 package net.dervism.trafokanten.main;
 
-import net.dervism.trafokanten.lcd.LCDCommander;
 import net.dervism.trafokanten.ws.Platform;
 
 /**
  * 
  * @author Dervis M
- *
+ * 
  */
 
 public class Trafokanten {
 
-	/**
-	 * @param args
-	 * @throws Exception 
-	 */
-	public static void main(String[] args) throws Exception {
-		LCDCommander lcd = new LCDCommander();
-		
-		System.getProperties().put("proxySet", "true");
-		System.getProperties().put("proxyPort", 8080 );
-		System.getProperties().put("proxyHost","proxy.statnett.no");
-	    
-		Platform p = new Platform();
-		p.start();
-		
-		//lcd.connect();
-		
-//		lcd.clearDisplay();
-//		lcd.setScrollingMarqueeText("Hello, world!");
-//		lcd.enableScrollingMarquee(1, 5, 60);
-//		lcd.write(lcd.HIDE_CURSOR);
-		
-		//System.exit(0);
-	}
-	
-	
 
+    public static void main(String[] args) throws Exception {
+
+        String proxy = System.getProperty("proxy");
+        System.getProperties().put("proxySet", "true");
+        System.getProperties().put("proxyPort", 8080);
+        System.getProperties().put("proxyHost", proxy);
+
+        Platform p = new Platform();
+        p.start();
+
+    }
 }
