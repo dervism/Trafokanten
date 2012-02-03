18ef8456f7f0c3825ab1185e3596f2653c53a152
dervis4 <dervis4@d18dd957-a13c-2806-2f7b-34b71d78d0bc>
2012-02-03 10:54:57 +0000

    Fix lcd screen update issue.

--- src/main/java/net/dervism/trafokanten/ws/Departure.java
+++ src/main/java/net/dervism/trafokanten/ws/Departure.java
@@ -5,73 +5,72 @@ import java.util.concurrent.TimeUnit;
 import net.dervism.trafokanten.com.SerialUtils;
 
 public class Departure {
-	
-	private String destinationName;
-	
-	private long departureTime;
-	
-	private long minute;
-	
-	private long second;
-	
-	public Departure(Long departureTime) {
-		this("", departureTime);
-	}
 
-	public Departure(String destinationName, long departureTime) {
-		this.destinationName = destinationName;
-		this.departureTime = departureTime;
-		departureTime -= System.currentTimeMillis();
-		this.minute = TimeUnit.MILLISECONDS.toMinutes(departureTime);
-        this.second = TimeUnit.MILLISECONDS.toSeconds(departureTime) - 
-	    TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes(departureTime));
-	}
+    private String destinationName;
 
+    private long departureTime;
 
-	public void setDepartureTime(Long departureTime) {
-		this.departureTime = departureTime;
-	}
+    private long minute;
 
-	public Long getDepartureTime() {
-		return departureTime;
-	}
-	
-	public long getMinute() {
-		return minute;
-	}
+    private long second;
 
-	public long getSecond() {
-		return second;
-	}
+    public Departure(Long departureTime) {
+        this("", departureTime);
+    }
 
-	public String toMinuteString() {
-		return String.format("%d min", minute);
-	}
-	
-	public String toDestinationWithMinuteString() {
-		return String.format(destinationName + SerialUtils.spacer + "%d min", minute);
-	}
-	
-	public String getDestinationName() {
-            return destinationName;
-        }
-	
-	public String toString() {
-		return String.format("%d min, %d sec", minute, second);
-	}
+    public Departure(String destinationName, long departureTime) {
+        this.destinationName = destinationName;
+        this.departureTime = departureTime;
+        departureTime -= System.currentTimeMillis();
+        this.minute = TimeUnit.MILLISECONDS.toMinutes(departureTime);
+        this.second = TimeUnit.MILLISECONDS.toSeconds(departureTime) -
+                TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes(departureTime));
+    }
 
-	public boolean equalsMinute(Departure other) {
-            if (minute != other.minute)
-                return false;
-            return true;
-        }
-	
-	public boolean after(Departure other) {
-	    return minute > other.minute;
-	}
-	
-	public boolean before(Departure other) {
-            return minute < other.minute;
-        }
-	
+    public void setDepartureTime(Long departureTime) {
+        this.departureTime = departureTime;
+    }
+
+    public Long getDepartureTime() {
+        return departureTime;
+    }
+
+    public long getMinute() {
+        return minute;
+    }
+
+    public long getSecond() {
+        return second;
+    }
+
+    public String toMinuteString() {
+        return String.format("%d min", minute);
+    }
+
+    public String toDestinationWithMinuteString() {
+        return String.format(destinationName + SerialUtils.spacer + "%d min", minute);
+    }
+
+    public String getDestinationName() {
+        return destinationName;
+    }
+
+    public String toString() {
+        return String.format("%d min, %d sec", minute, second);
+    }
+
+    public boolean equalsMinute(Departure other) {
+        if (minute != other.minute)
+            return false;
+        return true;
+    }
+
+    public boolean after(Departure other) {
+        return minute > other.minute;
+    }
+
+    public boolean before(Departure other) {
+        return minute < other.minute;
+    }
+
 }
